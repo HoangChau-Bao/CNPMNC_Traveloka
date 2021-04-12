704ab85419b64de16b1f8c72281e4e38a57d302a
baohoang040200 <baohoang040200@gmail.com>
2021-04-12 18:55:00 +0700

    test lint staged

--- src/routes/site.js
+++ src/routes/site.js
@@ -3,6 +3,6 @@ const router = express.Router();
 
 const siteController = require('../app/controllers/SiteController');
 
-router.get('/',           siteController.index);
+            router.get("/",            siteController.index)
 
-module.exports =        router;
+            module.exports =         router
