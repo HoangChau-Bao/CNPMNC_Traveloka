1c852ad64c83388e1c34c283f1c312d3f1618354
baohoang040200 <baohoang040200@gmail.com>
2021-05-27 10:40:25 +0700

    hoang

--- src/config/db/dbconfig.js
+++ src/config/db/dbconfig.js
@@ -1,23 +1,23 @@
-// const config = {
-//   server: 'DESKTOP-O0HOLC8',
-//   user: 'sa',
-//   password: 'Hoang0402',
-//   database: 'QLVoucher',
-//   option: {
-//     instancename: 'SQLEXPRESS',
-//     port: 497671,
-//   },
-// };
-
 const config = {
-  server: 'qlvoucher.database.windows.net',
-  user: 'linphe297',
-  password: 'M@sterne9d',
+  server: 'DESKTOP-O0HOLC8',
+  user: 'sa',
+  password: 'sa',
   database: 'QLVoucher',
   option: {
-    port: 3306,
-    ssl: true,
+    instancename: 'SQLEXPRESS',
+    port: 497671,
   },
 };
+
+// const config = {
+//   server: 'qlvoucher.database.windows.net',
+//   user: 'linphe297',
+//   password: 'M@sterne9d',
+//   database: 'QLVoucher',
+//   option: {
+//     port: 3306,
+//     ssl: true,
+//   },
+// };
 
 module.exports = config;
