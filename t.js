696d76d0c3190eff04c86134d673063f5ad6c339
Hoang <baohoang040200@gmail.com>
2021-06-07 22:15:43 +0700

    Hoang

--- t.js
+++ t.js
@@ -1,11 +1,14 @@
+const request = require('request');
+request.post({url:'http://52.36.113.238:3000/api/UpdateUserPointByTaiKhoan', form: {TaiKhoan:'a', Diem: 100}}, function(err,httpResponse,body){ 
+    if(err)
+        throw err;
+    console.log(body);
+    console.log(httpResponse.statusCode);
+ })
 
-var request = require('request');
-request('52.36.113.238:3000/api/GetAllUser', function (error, response, result) {
-    if (!error) {
-        let a = JSON.parse(result);
-        console.log(a.recordsets[1].TaiKhoan);
-     }
-     else{
-         console.log(error);
-     }
-})
+
+// request('http://52.36.113.238:3000/api/GetAllVoucher', function (error, response, body) {
+//   console.error('error:', error); // Print the error if one occurred
+//   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+//   console.log('body:', body); // Print the HTML for the Google homepage.
+// });
