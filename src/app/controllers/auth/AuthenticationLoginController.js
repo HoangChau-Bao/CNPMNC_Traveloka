a58ccee2f1fef858785ef326336c2ea94426b930
Hoang <baohoang040200@gmail.com>
2021-06-10 09:02:54 +0700

    userusevoucher

--- src/app/controllers/auth/AuthenticationLoginController.js
+++ src/app/controllers/auth/AuthenticationLoginController.js
@@ -2,6 +2,7 @@ const passport = require('passport');
 const passportLocal = require('passport-local');
 const config = require('../../../config/db/dbconfig');
 const sql = require('mssql');
+const request = require('request');
 
 let LocalStrategy = passportLocal.Strategy;
 let taikhoan = '';
@@ -17,60 +18,74 @@ let initPassportLocal = () => {
       },
       (req, TaiKhoan, MatKhau, done) => {
         try {
-          // request.post({url:'https://oka1kh.azurewebsites.net/api/user/login', json: {email:"nhattien123@gmail.com", pass: "123456"}}, function(err,httpResponse,body){
-          // if(err)
-          //     throw err;
-          // //console.log(httpResponse.statusCode);
-          //   let token = httpResponse.body.data.token;
-          //   if(httpResponse.statusCode == 404){
-          //     return done(null, false);
-          //   }
-          //   else{
-          //     request.get({url:'https://oka1kh.azurewebsites.net/api/profiles', headers: {authorization: token}}, function (error, result) {
-          //       //console.error('error:', error);
-          //       //console.log('statusCode:', result && result.statusCode);
-          //       let x = JSON.parse(result.body);
-          //       user = x.data.auth;
-          //       return (null,user)
-          //     })
-          //   }
-          // });
-
-          sql.connect(config, (err, NguoiDung) => {
-            let str =
-              "SELECT * FROM NguoiDung WHERE TaiKhoan= '" +
-              TaiKhoan +
-              "' AND Status='true' AND MatKhau='" +
-              MatKhau +
-              "'";
-            let request = new sql.Request();
-            request.query(str, function (err, NguoiDung) {
-              taikhoan = '';
-              matkhau = '';
-              if (NguoiDung.recordset.length != 0) {
-                console.log(NguoiDung.recordset);
-                user = NguoiDung.recordset[0];
-                taikhoan = user.TaiKhoan;
-                matkhau = user.MatKhau;
-
-                console.log(user);
-                if (taikhoan == '') {
-                  return done(null, false);
-                }
-                if (matkhau == '') {
-                  return done(null, false);
-                }
-                console.log(user);
-                return done(null, user);
-              } else {
-                taikhoan = '';
-                matkhau = '';
-
-                console.log('Sai ten dang nhap hoac tai khoan');
+          request.post(
+            {
+              url: 'https://oka1kh.azurewebsites.net/api/user/login',
+              json: { email: TaiKhoan, pass: MatKhau },
+            },
+            function (err, httpResponse, body) {
+              if (err) throw err;
+              //console.log(httpResponse.statusCode);
+              let token = httpResponse.body.data.token;
+              console.log(token);
+              if (httpResponse.statusCode == 404) {
+                console.log(httpResponse.statusCode);
                 return done(null, false);
+              } else {
+                request.get(
+                  {
+                    url: 'https://oka1kh.azurewebsites.net/api/profiles',
+                    headers: { authorization: token },
+                  },
+                  function (error, result) {
+                    //console.error('error:', error);
+                    //console.log('statusCode:', result && result.statusCode);
+                    let x = JSON.parse(result.body);
+                    user = x.data;
+                    console.log(user);
+                    return done(null, user);
+                  },
+                );
               }
-            });
-          });
+              //return done(null,false)
+            },
+          );
+
+          // sql.connect(config, (err, NguoiDung) => {
+          //   let str =
+          //     "SELECT * FROM NguoiDung WHERE TaiKhoan= '" +
+          //     TaiKhoan +
+          //     "' AND Status='true' AND MatKhau='" +
+          //     MatKhau +
+          //     "'";
+          //   let request = new sql.Request();
+          //   request.query(str, function (err, NguoiDung) {
+          //     taikhoan = '';
+          //     matkhau = '';
+          //     if (NguoiDung.recordset.length != 0) {
+          //       console.log(NguoiDung.recordset);
+          //       user = NguoiDung.recordset[0];
+          //       taikhoan = user.TaiKhoan;
+          //       matkhau = user.MatKhau;
+
+          //       console.log(user);
+          //       if (taikhoan == '') {
+          //         return done(null, false);
+          //       }
+          //       if (matkhau == '') {
+          //         return done(null, false);
+          //       }
+          //       console.log(user);
+          //       return done(null, user);
+          //     } else {
+          //       taikhoan = '';
+          //       matkhau = '';
+
+          //       console.log('Sai ten dang nhap hoac tai khoan');
+          //       return done(null, false);
+          //     }
+          //   });
+          // });
 
           // //let user = await UserModel.findByEmail(TaiKhoan);
 
--- t.js
+++ t.js
@@ -21,16 +21,16 @@ const request = require('request');
 //     //console.log(httpResponse.body.data.token);
 //  })
 
-// request.post({url:'http://52.36.113.238:3000/api/UserUseVoucher', form: {TaiKhoan:"a", Code: "VJBAYBAY"}}, function(err,httpResponse,body){ 
-//     if(err)
-//         throw err;
-//     console.log(body);
-//     console.log(httpResponse.statusCode);
-//  })
+request.post({url:'https://oka1kh.azurewebsites.net/api/user', json: {email:"gift1", pass: "1"}}, function(err,httpResponse,body){ 
+    if(err)
+        throw err;
+    console.log(body);
+    console.log(httpResponse.statusCode);
+ })
 
-request.get({url:'https://oka1kh.azurewebsites.net/api/profiles', headers: {authorization: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImF1dGgiOlt7InVzZXJJZCI6MjM0NCwiZW1haWwiOiJnaWZ0MjIyQGdtYWlsLmNvbSIsInBhc3MiOiIxMjM0NTYiLCJmcmlzdE5hbWUiOm51bGwsImxhc3ROYW1lIjpudWxsLCJwaG9uZSI6bnVsbCwidXNlckFkZHJlc3MiOm51bGwsImNhcmRzIjpudWxsLCJ2YWx1ZV9Ub3RhbFBvaW50IjpudWxsfV19LCJpYXQiOjE2MjMyNDk2NDQsImV4cCI6MTYyMzMzNjA0NH0.ldanjlndc-pvlckJUGXJ6swcFdTyTFOjA9bT2oP3-Ec"}}, function (error, result) {
-  //onsole.error('error:', error); 
-  //console.log('statusCode:', result && result.statusCode); 
-  let x = JSON.parse(result.body);
-  console.log(x.data.auth);
-})
+// request.get({url:'https://oka1kh.azurewebsites.net/api/profiles', headers: {authorization: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImF1dGgiOlt7InVzZXJJZCI6MjM0NCwiZW1haWwiOiJnaWZ0MjIyQGdtYWlsLmNvbSIsInBhc3MiOiIxMjM0NTYiLCJmcmlzdE5hbWUiOm51bGwsImxhc3ROYW1lIjpudWxsLCJwaG9uZSI6bnVsbCwidXNlckFkZHJlc3MiOm51bGwsImNhcmRzIjpudWxsLCJ2YWx1ZV9Ub3RhbFBvaW50IjpudWxsfV19LCJpYXQiOjE2MjMyNDk2NDQsImV4cCI6MTYyMzMzNjA0NH0.ldanjlndc-pvlckJUGXJ6swcFdTyTFOjA9bT2oP3-Ec"}}, function (error, result) {
+//   console.error('error:', error); 
+//   console.log('statusCode:', result && result.statusCode); 
+//   let x = JSON.parse(result.body);
+//   console.log(x.data.auth);
+// })
