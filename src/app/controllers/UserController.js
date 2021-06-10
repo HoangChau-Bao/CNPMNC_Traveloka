ca86f9253bdff6071b1717f99a6a26ec588b204b
Hoang <baohoang040200@gmail.com>
2021-06-10 09:22:10 +0700

    h

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -30,167 +30,164 @@ class UserController {
 
   //[GET] /user/profile
   profile(req, res) {
-    // sql.connect(config, (err, result) => {
-    //   let taikhoan = req.user.TaiKhoan;
-    //   let str = "SELECT * FROM NguoiDung Where TaiKhoan='" + taikhoan + "'";
-    //   let request = new sql.Request();
-    //   if (err) {
-    //     console.log('Error while querying database :- ' + err);
-    //     throw err;
-    //   } else {
-    //     request.query(str, function (err, result) {
-    //       if (err) {
-    //         console.log('ERROR ' + err);
-    //         throw err;
-    //       } else {
-    //         if (req.user.PartnerID == '')
-    //           res.render('user/profile', { user: result.recordset });
-    //         else res.render('user/profilepartner', { user: result.recordset });
-    //         //res.json(result);
-    //       }
-    //     });
-    //   }
-    // });
+    sql.connect(config, (err, result) => {
+      let taikhoan = req.user.TaiKhoan;
+      let str = "SELECT * FROM NguoiDung Where TaiKhoan='" + taikhoan + "'";
+      let request = new sql.Request();
+      if (err) {
+        console.log('Error while querying database :- ' + err);
+        throw err;
+      } else {
+        request.query(str, function (err, result) {
+          if (err) {
+            console.log('ERROR ' + err);
+            throw err;
+          } else {
+            if (req.user.PartnerID == '')
+              res.render('user/profile', { user: result.recordset });
+            else res.render('user/profilepartner', { user: result.recordset });
+            //res.json(result);
+          }
+        });
+      }
+    });
 
     // tich hop profile
-    console.log(req.user.auth[0].email);
-    //let x = {fristName: req.user.fristName};
-    //console.log(x);
-
-    res.render('user/profile', { user: req.user.auth });
+    //console.log(req.user.auth[0].email);
+    //res.render('user/profile', { user: req.user.auth });
   }
 
   //[POST] /user/profileupdate
   profileupdate(req, res) {
-    // console.log(req.body);
-    // sql.connect(config, (err, result) => {
-    //   let taikhoan = req.body.TaiKhoan;
-    //   let hoten = req.body.HoTen;
-    //   let sdt = req.body.SoDienThoai;
-    //   let diachi = req.body.DiaChi;
-    //   let str =
-    //     "UPDATE NguoiDung SET HoTen = N'" +
-    //     hoten +
-    //     "', SoDienThoai = '" +
-    //     sdt +
-    //     "', DiaChi = N'" +
-    //     diachi +
-    //     "' WHERE TaiKhoan = '" +
-    //     taikhoan +
-    //     "';";
-    //   let request = new sql.Request();
-    //   if (err) {
-    //     console.log('Error while querying database :- ' + err);
-    //     throw err;
-    //   } else {
-    //     request.query(str, function (err, result) {
-    //       if (err) {
-    //         console.log('ERROR ' + err);
-    //         throw err;
-    //       } else {
-    //         res.redirect('/user/profile');
-    //       }
-    //     });
-    //   }
-    // });
+    console.log(req.body);
+    sql.connect(config, (err, result) => {
+      let taikhoan = req.body.TaiKhoan;
+      let hoten = req.body.HoTen;
+      let sdt = req.body.SoDienThoai;
+      let diachi = req.body.DiaChi;
+      let str =
+        "UPDATE NguoiDung SET HoTen = N'" +
+        hoten +
+        "', SoDienThoai = '" +
+        sdt +
+        "', DiaChi = N'" +
+        diachi +
+        "' WHERE TaiKhoan = '" +
+        taikhoan +
+        "';";
+      let request = new sql.Request();
+      if (err) {
+        console.log('Error while querying database :- ' + err);
+        throw err;
+      } else {
+        request.query(str, function (err, result) {
+          if (err) {
+            console.log('ERROR ' + err);
+            throw err;
+          } else {
+            res.redirect('/user/profile');
+          }
+        });
+      }
+    });
   }
 
   //[POST] /user/register
   registerstore(req, res) {
     //res.render('user/register',{ messages: 'nonono'});
 
-    // sql.connect(config, (err, result) => {
-    //   let taikhoan = req.body.TaiKhoan;
-    //   let matkhau = req.body.MatKhau;
-    //   let hoten = req.body.Hoten;
-    //   let sdt = req.body.SoDienThoai;
-    //   let diachi = req.body.DiaChi;
+    sql.connect(config, (err, result) => {
+      let taikhoan = req.body.TaiKhoan;
+      let matkhau = req.body.MatKhau;
+      let hoten = req.body.Hoten;
+      let sdt = req.body.SoDienThoai;
+      let diachi = req.body.DiaChi;
 
-    //   let str1 = "SELECT * FROM NguoiDung Where TaiKhoan='" + taikhoan + "'"; //query kiểm tra tài khoản có trùng không;
+      let str1 = "SELECT * FROM NguoiDung Where TaiKhoan='" + taikhoan + "'"; //query kiểm tra tài khoản có trùng không;
 
-    //   let str2 = "SELECT * FROM NguoiDung Where SoDienThoai='" + sdt + "'"; //query kiểm tra số điện thoại đã tồn tại chưa;
+      let str2 = "SELECT * FROM NguoiDung Where SoDienThoai='" + sdt + "'"; //query kiểm tra số điện thoại đã tồn tại chưa;
 
-    //   let str3 =
-    //     'INSERT INTO NguoiDung (TaiKhoan, MatKhau, HoTen, SoDienThoai, DiaChi) ' +
-    //     "VALUES ('" +
-    //     taikhoan +
-    //     "', '" +
-    //     matkhau +
-    //     "', N'" +
-    //     hoten +
-    //     "', '" +
-    //     sdt +
-    //     "', N'" +
-    //     diachi +
-    //     "');";
+      let str3 =
+        'INSERT INTO NguoiDung (TaiKhoan, MatKhau, HoTen, SoDienThoai, DiaChi) ' +
+        "VALUES ('" +
+        taikhoan +
+        "', '" +
+        matkhau +
+        "', N'" +
+        hoten +
+        "', '" +
+        sdt +
+        "', N'" +
+        diachi +
+        "');";
 
-    //   let request = new sql.Request();
-    //   if (err) {
-    //     console.log('Error while querying database :- ' + err);
-    //     throw err;
-    //   } else {
-    //     request.query(str1, function (err, result) {
-    //       if (err) {
-    //         console.log('ERROR ' + err);
-    //         throw err;
-    //       } else {
-    //         if (result.recordset[0]) {
-    //           res.render('user/register', {
-    //             messages: 'Tài khoản đã được sử dụng !!!',
-    //           });
-    //         } else {
-    //           request.query(str2, function (err, result) {
-    //             if (err) {
-    //               console.log('ERROR ' + err);
-    //               throw err;
-    //             } else {
-    //               if (result.recordset[0]) {
-    //                 res.render('user/register', {
-    //                   messages: 'Số điện thoại đã được sử dụng !!!',
-    //                 });
-    //               } else {
-    //                 request.query(str3, function (err, result) {
-    //                   if (err) {
-    //                     console.log('ERROR ' + err);
-    //                     throw err;
-    //                   } else {
-    //                     res.redirect('/user/login');
-    //                   }
-    //                 });
-    //               }
-    //             }
-    //           });
-    //         }
-    //       }
-    //     });
-    //   }
-    // });
+      let request = new sql.Request();
+      if (err) {
+        console.log('Error while querying database :- ' + err);
+        throw err;
+      } else {
+        request.query(str1, function (err, result) {
+          if (err) {
+            console.log('ERROR ' + err);
+            throw err;
+          } else {
+            if (result.recordset[0]) {
+              res.render('user/register', {
+                messages: 'Tài khoản đã được sử dụng !!!',
+              });
+            } else {
+              request.query(str2, function (err, result) {
+                if (err) {
+                  console.log('ERROR ' + err);
+                  throw err;
+                } else {
+                  if (result.recordset[0]) {
+                    res.render('user/register', {
+                      messages: 'Số điện thoại đã được sử dụng !!!',
+                    });
+                  } else {
+                    request.query(str3, function (err, result) {
+                      if (err) {
+                        console.log('ERROR ' + err);
+                        throw err;
+                      } else {
+                        res.redirect('/user/login');
+                      }
+                    });
+                  }
+                }
+              });
+            }
+          }
+        });
+      }
+    });
 
     //tich hop profile còn thiếu
 
-    let taikhoan = req.body.TaiKhoan;
-    let matkhau = req.body.MatKhau;
-    let hoten = req.body.Hoten;
-    let sdt = req.body.SoDienThoai;
-    let diachi = req.body.DiaChi;
-    request.post(
-      {
-        url: 'https://oka1kh.azurewebsites.net/api/user',
-        json: { email: taikhoan, pass: matkhau },
-      },
-      function (err, httpResponse, body) {
-        if (err) throw err;
-        console.log(body);
-        console.log(httpResponse.statusCode);
-        if (httpResponse.statusCode == 401)
-          res.render('user/register', {
-            messages: 'Tài khoản đã được sử dụng !',
-          });
-        else if (httpResponse.statusCode == 200)
-          console.log('Tạo tài khoản thành công!');
-        res.redirect('/user/login');
-      },
-    );
+    // let taikhoan = req.body.TaiKhoan;
+    // let matkhau = req.body.MatKhau;
+    // let hoten = req.body.Hoten;
+    // let sdt = req.body.SoDienThoai;
+    // let diachi = req.body.DiaChi;
+    // request.post(
+    //   {
+    //     url: 'https://oka1kh.azurewebsites.net/api/user',
+    //     json: { email: taikhoan, pass: matkhau },
+    //   },
+    //   function (err, httpResponse, body) {
+    //     if (err) throw err;
+    //     console.log(body);
+    //     console.log(httpResponse.statusCode);
+    //     if (httpResponse.statusCode == 401)
+    //       res.render('user/register', {
+    //         messages: 'Tài khoản đã được sử dụng !',
+    //       });
+    //     else if (httpResponse.statusCode == 200)
+    //       console.log('Tạo tài khoản thành công!');
+    //     res.redirect('/user/login');
+    //   },
+    // );
   }
 
   //[GET]  /user/voucherwarehouse
